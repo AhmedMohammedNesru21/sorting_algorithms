1f8b98d9a31faf1dfc2fb00cbe75c3388edd11b0
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-11-06 04:10:59 +0000

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,25 @@
 #include "sort.h"
-
 /**
- * swap - swap array elements
- * @xp: ptr to array element
- * @yp: prt to array element
- */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-/**
- * bubble_sort - sort an array of integers asending order
- * @array: input array
- * @size: size of array
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	char isSorted;
 
-	if (array == NULL || size < 2)
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
-	{
-		isSorted = 1;
-		for (j = 0; j < size - i - 1; j++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				swap(&array[j], &array[j + 1]);
-				isSorted = 0;
-				print_array(array, size);
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		/* if no two elements were swapped by inner loop, then break */
-		if (isSorted == 1)
-			break;
-	}
 }
