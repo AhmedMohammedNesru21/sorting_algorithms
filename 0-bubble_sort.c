4f85e99bb61c6b535112badf92ac6bfd70386c4c
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-11-06 04:31:05 +0000

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,25 @@
 #include "sort.h"
-void swap(int *a, int *b);
 /**
- * bubble_sort - sort an array of integer in ascending order
- * @array: the array to order and print
- * @size: size of array to order
- */
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0;
+	size_t i, j, n;
 
-	if (size <= 1)
-		return;
-
-	for (i = 0; i < (size - 1); i++)
-		for (j = 1; j < size; j++)
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		{
 			if (array[j - 1] > array[j])
 			{
-				swap(&array[j - 1], &array[j]);
+				n = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = n;
 				print_array(array, size);
 			}
-}
-
-/**
- * swap - swap two pointers to integer
- *
- * @a: first int pointer
- * @b: second int pointer
- */
-void swap(int *a, int *b)
-{
-	int swap;
-
-	swap = *a;
-	*a = *b;
-	*b = swap;
+		}
+	}
 }
